9f32632bdfbcf7d4ebde314d01f0fcd7ab03b304
ShendRudari <sr64786@ubt-uni.net>
2026-01-23 09:38:58 +0100

    feat: install EF Core packages and add Guest model

---
+++ src/EliteResort.API/Models/Guest.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EliteResort.API.Models
+{
+    public class Guest
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(50)]
+        public string LastName { get; set; } = string.Empty;
+
+        [EmailAddress]
+        public string? Email { get; set; }
+
+        public string? PhoneNumber { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
