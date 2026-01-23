a89d912cc6996f0daf8a016a53c61faf68f3151c
ShendRudari <sr64786@ubt-uni.net>
2026-01-23 10:02:49 +0100

    feat: added GuestsController manually

---
+++ src/EliteResort.API/Controllers/GuestsController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using EliteResort.API.Data;
+using EliteResort.API.Models;
+
+namespace EliteResort.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GuestsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public GuestsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Guest>>> GetGuests()
+        {
+            return await _context.Guests.ToListAsync();
+        }
+
+       
+        [HttpPost]
+        public async Task<ActionResult<Guest>> PostGuest(Guest guest)
+        {
+            _context.Guests.Add(guest);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetGuests), new { id = guest.Id }, guest);
+        }
+    }
+}
