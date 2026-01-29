3434e8b34c1745a857a34762380f2246e05a76f1
drinche <qitakudrini@gmail.com>
2026-01-29 02:09:11 +0100

    Krijimi i RoomTypeComponents dhe RoomTypelist

--- eliteresort-frontend/src/App.jsx
+++ eliteresort-frontend/src/App.jsx
@@ -5,20 +5,20 @@ import GuestList from "./pages/Guests/GuestList";
 import RoomList from "./pages/Rooms/RoomList";
 import MenuList from "./pages/Restaurant/MenuList";
 import BookingList from "./pages/Bookings/BookingList";
-
+import RoomTypeList from "./pages/RoomTypes/RoomTypeList";
 function App() {
   return (
     <Router>
       <div className="flex min-h-screen bg-gray-100">
-        {/* Sidebar qëndron fiks në të majtë */}
         <Sidebar />
 
-        {/* Pjesa kryesore e përmbajtjes */}
         <main className="flex-1 p-8">
           <Routes>
             <Route path="/" element={<Dashboard />} />
             <Route path="/guests" element={<GuestList />} />
             <Route path="/rooms" element={<RoomList />} />
+            {/* 2. SHTO KËTË RRESHT KËTU */}
+            <Route path="/room-types" element={<RoomTypeList />} /> 
             <Route path="/restaurant" element={<MenuList />} />
             <Route path="/bookings" element={<BookingList />} />
           </Routes>
